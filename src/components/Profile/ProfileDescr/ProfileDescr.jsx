4bbd243216592328082ec5284eef882ad40f2c7b
Yurii Shchepilov <89077590+yurilov@users.noreply.github.com>
2022-04-30 11:27:54 +0300

    added profile description

---
+++ src/components/Profile/ProfileDescr/ProfileDescr.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ProfileDescr.module.css';
+import defaultAvatar from './defaultAvatar.jpg';
+
+function ProfileDescription({
+    username, 
+    tag,
+    location,
+    avatar = defaultAvatar,
+}) {
+    return (
+        <div className={styles.description}>
+            <img src={avatar} alt="User avatar" className={styles.avatar}/>
+            <p className={styles.name}>{username}</p>
+            {tag && <p className={styles.tag}>@{tag}</p>}
+            {location && <p className={styles.location}>@{location}</p>}
+
+        </div>
+    )
+}
+export default ProfileDescription;
